e6d0c108aef5358ecee9079eed37002f9c675d8b
aperezherrera <aperezherrera@nevada.unr.edu>
2023-04-03 15:12:24 -0700

    Added basic fuctionality for insert

--- driver.py
+++ driver.py
@@ -36,7 +36,7 @@ def main():
             else: # If the string initially ends in a semicolon, remove it
                 inputTokens[-1] = inputTokens[-1][:-1]
             
-            #print(inputTokens)
+            print(inputTokens)
             recognizeInput(inputTokens)
 
 def recognizeInput(inputTokens):
@@ -44,21 +44,31 @@ def recognizeInput(inputTokens):
     if command == ".exit":
         print("All done.")
         quit()
-    elif command == "CREATE":
-        createCommand(inputTokens)
-    elif command == "DROP":
-        dropCommand(inputTokens)
-    elif command == "SELECT":
+    elif command == "CREATE" or command == "create":
+        if(len(inputTokens) >= 3):
+            createCommand(inputTokens)
+        else:
+            print("Error: Invalid syntax. Please try again.")
+            return 1
+    elif command == "DROP" or command == "drop":
+        if(len(inputTokens) == 3):
+            dropCommand(inputTokens)
+            
+        else:
+            print("Error: Invalid syntax. Please try again.")
+            return 1
+    elif command == "SELECT" or command == "select":
         selectCommand(inputTokens)
-    elif command == "USE":
+    elif command == "USE" or command == "use":
         if(len(inputTokens) == 2):
-            print("Use command")
+            #print("Use command")
             useCommand(inputTokens)
         else:
             print("Error: Invalid syntax. Please try again.")
             return 1
-    elif command == "INSERT":
-        insertCommand(inputTokens)
+    elif command == "INSERT" or command == "insert":
+        if(len(inputTokens) >= 4):
+            insertCommand(inputTokens)
     else:
         print("Errror: Invalid command. Please try again.\n")
     return 0
@@ -83,11 +93,11 @@ def initializeDatabase(inputTokens):
     print("Database {0} created.".format(inputTokens[2]))
     return 0
 
-
 def initializeTable(inputTokens):
     global workingPath
     global useCheck
-    print("Initialize table")
+    tableParameters = []
+    #print("Initialize table")
     if(useCheck == False): # If no database is in use, return an error
         print("Error: No database in use. Please try again.")
         return 1
@@ -95,12 +105,38 @@ def initializeTable(inputTokens):
         print("!Failed to create table {0} because it already exists.".format(inputTokens[2]))
         return 1
     else: # If the table does not exist, create it
-        open((workingPath + "/" + inputTokens[2]), 'w')
+        tableFile = open((workingPath + "/" + inputTokens[2]), 'w')
         print("Table {0} created.".format(inputTokens[2]))
+    #Copy the table parameters into a list
+    for i in range(3, len(inputTokens)):
+        tableParameters.append(inputTokens[i])
+    #print(tableParameters)
+    #Remove the parentheses from the first and last elements
+    tableParameters[0] = tableParameters[0][1:]
+    tableParameters[-1] = tableParameters[-1][:-1]
+    #print(tableParameters)
+    # Remove the commas from the middle elements
+    for i in range(0, len(tableParameters) - 2):
+        if(tableParameters[i][-1] == ","):
+            tableParameters[i] = tableParameters[i][:-1]
+    #print(tableParameters)
+    # Add spaces between the elements
+    for i in range(0, len(tableParameters)):
+        tableParameters[i] = tableParameters[i] + " "
+    #print(tableParameters)
+    # Add | symbols between every other element
+    for i in range(1, len(tableParameters) - 1, 2):
+        tableParameters[i] = tableParameters[i] + "|" + " "
+    #print(tableParameters)
+    tableFile.writelines(tableParameters)
+    tableFile.close()
     return 0
 
 def dropCommand(inputTokens):
-    print("Drop command")
+    print("Functionality not yet implemented.")
+    #if inputTokens[1] == "DATABASE":
+        #os.rmdir(workingPath + "/" + inputTokens[2])
+    return 1
 
 def useCommand(inputTokens):
     global workingPath
@@ -117,10 +153,57 @@ def useCommand(inputTokens):
     return 0
 
 def selectCommand(inputTokens):
-    print("Select command")
+    #print("Select command")
+    return 0
 
 def insertCommand(inputTokens):
-    print("Insert command")
+    global workingPath
+    global useCheck
+    if(useCheck == False):
+        print("Error: No database in use. Please try again.")
+        return 1
+    if(os.path.isfile(workingPath + "/" + inputTokens[2])):
+        tableFile = open(workingPath + "/" + inputTokens[2], 'a')
+        tableFile.write("\n")
+
+        #Clean up the input string
+        record = []
+        for i in range(3, len(inputTokens)):
+            record.append(inputTokens[i])
+        print(record)
+        #remove the word "values" from the first element
+        record[0] = record[0][6:]
+        print(record)
+        #remove the parentheses from the first and last elements
+        record[0] = record[0][1:]
+        record[-1] = record[-1][:-1]
+        print(record)
+        #remove commas from all elements
+        for i in range(0, len(record)):
+            if(record[i][-1] == ","):
+                record[i] = record[i][:-1]
+        print(record)
+        # remove the quotation marks from all elements
+        for i in range(0, len(record)):
+            if(record[i][0] == "'"):
+                record[i] = record[i][1:]
+            if(record[i][-1] == "'"):
+                record[i] = record[i][:-1]
+        print(record)
+        #add spaces between the elements
+        for i in range(0, len(record)):
+            record[i] = record[i] + " "
+        print(record)
+        #add | symbols between all elements except the last
+        for i in range(0, len(record) - 1):
+            record[i] = record[i] + "|" + " "
+        print(record)
+
+        #Write the record to the table
+        for i in range(0, len(record)):
+            tableFile.write(record[i])
+        tableFile.close()
+    return 0
 
 if __name__ == "__main__":
     main()
